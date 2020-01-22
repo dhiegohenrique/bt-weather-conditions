c92485fba66ed103a80feceb2ff256920080cc5e
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-01-22 13:17:29 -0300

    add date.mixin

---
+++ src/shared/mixins/date.mixin.js
@@ -0,0 +1,15 @@
+import moment from 'moment-timezone'
+moment.locale('pt-BR')
+
+const mixin = {
+  methods: {
+    getFormattedDateHour (date) {
+      return moment(date).format('dddd, DD/MM/YYYY HH:mm:ss')
+    },
+    getFormattedHour (date) {
+      return moment.unix(date).tz('America/Sao_Paulo').format('HH:mm')
+    }
+  }
+}
+
+export default mixin
