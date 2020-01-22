acffe1ea19452dac6febcf0d44782c7f8d27a15d
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-01-22 13:17:47 -0300

    add weather card

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -1,5 +1,6 @@
 import WeatherMixin from '@/shared/mixins/weather.mixin'
 import GeolocationMixin from '@/shared/mixins/geolocation.mixin'
+import WeatherCard from '@/components/weather-card/index'
 
 export default {
   name: 'home',
@@ -7,15 +8,23 @@ export default {
     WeatherMixin,
     GeolocationMixin
   ],
+  components: {
+    WeatherCard
+  },
+  data () {
+    return {
+      weatherConditions: null
+    }
+  },
   methods: {
     async search () {
       try {
         this.$root.$emit('showLoading')
         const geolocation = await this.getGeolocation('150+Humberto+de+Campos,88036-420')
-        const weatherConditions = await this.getWeatherConditions(geolocation.lat, geolocation.lon)
+        this.weatherConditions = await this.getWeatherConditions(geolocation.lat, geolocation.lon)
 
         // eslint-disable-next-line no-console
-        console.log('res: ' + JSON.stringify(weatherConditions))
+        console.log('res: ' + JSON.stringify(this.weatherConditions))
       } finally {
         this.$root.$emit('hideLoading')
       }
