c721176ccd1ce4880df63068b80980272117a0d8
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-01-23 12:02:21 -0300

    add toast

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import LoadingModal from '@/components/loading-modal/index'
+import Toast from '@/components/toast/index'
 
 export default {
   name: 'app',
   components: {
-    LoadingModal
+    LoadingModal,
+    Toast
   }
 }
