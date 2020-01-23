6fa013e24e769195a24314cce424831f77752638
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-01-23 17:02:43 -0300

    add lodash in vue

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router/index'
 import { store } from './store/store'
 import Toasted from 'vue-toasted'
+import VueLodash from 'vue-lodash'
 import vuetify from './plugins/vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 Vue.use(Toasted, { singleton: true })
+Vue.use(VueLodash)
 
 new Vue({
   vuetify,
