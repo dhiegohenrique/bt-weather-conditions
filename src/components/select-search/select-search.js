0cf558b8fdb1cae3bbe45286706b1906447ed7d5
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-01-23 12:05:14 -0300

    add validate item

--- src/components/select-search/select-search.js
+++ src/components/select-search/select-search.js
@@ -24,6 +24,10 @@ export default {
     minCharSearch: {
       type: Number,
       default: 3
+    },
+    hasValidateItem: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
@@ -65,22 +69,41 @@ export default {
     selectItem (item) {
       this.searchValue = item
       this.showPopup = false
+      // eslint-disable-next-line no-console
+      console.log('selecionou o item: ' + item)
       this.$emit('selectItem', item)
     },
-    validateItem () {
-      const search = this.normalizeItem(this.searchValue)
-      const index = this.items.findIndex((item) => {
-        return search === this.normalizeItem(item)
-      })
+    validateItem (event) {
+      // // eslint-disable-next-line no-console
+      // console.log('entrou aqui1: ' + this.hasValidateItem)
+      // this.showPopup = false
 
-      this.showPopup = false
-      if (index === -1) {
-        this.searchValue = ''
-      } else {
-        this.searchValue = this.items[index]
-      }
+      // // eslint-disable-next-line no-debugger
+      // // debugger
+      // if (!this.hasValidateItem || event.relatedTarget.parentElement.className.includes('v-item-group')) {
+      //   return
+      // }
 
-      this.$emit('selectItem', this.searchValue)
+      // // eslint-disable-next-line no-console
+      // console.log('entrou aqui2')
+      // // eslint-disable-next-line no-debugger
+      // // debugger
+
+      // const search = this.normalizeItem(this.searchValue)
+      // const index = this.items.findIndex((item) => {
+      //   return search === this.normalizeItem(item)
+      // })
+
+      // if (index === -1) {
+      //   this.searchValue = ''
+      // } else {
+      //   this.searchValue = this.items[index]
+      // }
+
+      // this.$emit('selectItem', this.searchValue)
+    },
+    clear () {
+      this.searchValue = ''
     }
   }
 }
