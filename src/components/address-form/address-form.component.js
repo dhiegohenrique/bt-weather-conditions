14a208818faaf4e55958272532238169a1072230
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-01-24 21:41:00 -0300

    add address in history

--- src/components/address-form/address-form.component.js
+++ src/components/address-form/address-form.component.js
@@ -55,26 +55,24 @@ export default {
       })
     },
     async search () {
-      try {
-        this.$root.$emit('showLoading')
+      this.$root.$emit('showLoading')
 
-        const address = {
-          street: this.street,
-          number: this.number,
-          neighborhood: this.neighborhood,
-          cep: this.cep,
-          city: this.city,
-          state: this.state
-        }
+      const address = {
+        street: this.street,
+        number: this.number,
+        neighborhood: this.neighborhood,
+        cep: this.cep,
+        city: this.city,
+        state: this.state
+      }
 
-        const geolocation = await this.getGeolocation(address)
-        if (!geolocation) {
-          this.$root.$emit('showToast', 'Não foi encontrada uma localização com os dados informados.')
-        } else {
-          this.$emit('setGeolocation', geolocation)
-        }
-      } finally {
+      const geolocation = await this.getGeolocation(address)
+      if (!geolocation) {
+        this.$root.$emit('showToast', 'Não foi encontrada uma localização com os dados informados.')
         this.$root.$emit('hideLoading')
+      } else {
+        this.$emit('setGeolocation', geolocation)
+        this.addInHistory(address)
       }
     },
     clear () {
@@ -84,8 +82,50 @@ export default {
       this.cep = ''
       this.city = ''
       this.state = ''
-      this.$refs['select-state'].clear()
+
+      const weatherForm = this.$refs['weather-form']
+      const children = weatherForm.$children
+      children.forEach((item) => {
+        if (item.clear) {
+          item.clear()
+        }
+      })
+
       this.$emit('clear')
+    },
+    setValue (field, value) {
+      this[field] = value
+    },
+    addInHistory (address) {
+      const items = [
+        {
+          key: 'street',
+          value: this.streetHistory
+        },
+        {
+          key: 'number',
+          value: this.numberHistory
+        },
+        {
+          key: 'neighborhood',
+          value: this.neighborhoodHistory
+        },
+        {
+          key: 'cep',
+          value: this.cepHistory
+        },
+        {
+          key: 'city',
+          value: this.cityHistory
+        }
+      ]
+
+      items.forEach((item) => {
+        const addressValue = address[item.key]
+        if (addressValue && !item.value.includes(addressValue)) {
+          item.value.push(addressValue)
+        }
+      })
     }
   }
 }
